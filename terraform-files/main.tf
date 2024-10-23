0c93bfb5a6b5bc1d59ff8ff90ee60fa3b76eae86
amolborse2001 <115874482+amolborse2001@users.noreply.github.com>
2024-10-24 02:26:03 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,29 +1,29 @@
 resource "aws_instance" "test-server" {
-  ami                    = "ami-06b21ccaeff8cd686"
-  instance_type         = "t2.micro"
-  key_name              = "mypermanentkey"
-  vpc_security_group_ids = ["sg-047cdca1ff3d954c2"]  # Security group for launch-wizard-20
+  ami                    = "ami-06b21ccaeff8cd686"  # AMI ID for the instance
+  instance_type         = "t2.micro"                 # Instance type
+  key_name              = "mypermanentkey"           # Name of the key pair
+  vpc_security_group_ids = ["sg-047cdca1ff3d954c2", "sg-0da6313eb3762de38"]  # Security groups
 
   connection {
     type        = "ssh"
-    user       = "ec2-user"
-    private_key = file("./mypermanentkey.pem")
-    host       = self.public_ip
+    user        = "ec2-user"                         # User for SSH
+    private_key = file("./mypermanentkey.pem")      # Path to the private key file
+    host        = self.public_ip                      # Use the public IP of the instance
   }
 
   provisioner "remote-exec" {
-    inline = ["echo 'wait to start the instance'"]
+    inline = ["echo 'wait to start the instance'"]   # Command to execute after instance is up
   }
 
   tags = {
-    Name = "test-server"
+    Name = "test-server"                              # Tag for the instance
   }
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"  # Store IP in inventory file
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-files/ansibleplaybook.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-files/ansibleplaybook.yml"  # Run Ansible playbook
   }
 }
